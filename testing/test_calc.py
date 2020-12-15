939c9cac040a478ddc67df9ee28bb197c9662a04
dltsdr <852668587@qq.com>
2020-12-15 13:53:26 +0800

    修改

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -7,20 +7,41 @@ print(sys.path.append('..'))
 from pythoncode.calc import Calculator
 
 
-@pytest.mark.add
-def test_add():
-    cal = Calculator()
-    assert 2 == cal.add(1, 1)
+def setup_module():
+	print("模块级别setup")
+
+def teardown_module():
+	print("函数级别teardown")
+#函数级别
+def setup_function():
+	print("函数级别setup")
+
+def teardown_function():
+	print("函数级别teardown")
 
 
-@pytest.mark.add
-def test_add1():
-    cal = Calculator()
-    assert 3 == cal.add(1, 2)
+class Test_Calc:
+    def setup(self):
+        self.cal = Calculator
+        print("类级别setup")
+    def teardown(self):
+        print("类级别teardowns")
 
 
-@pytest.mark.div
-def test_div():
-    cal = Calculator()
-    assert 1 == cal.div(1, 1)
-print("a")
+
+	@pytest.mark.add
+	def test_add(self):
+		#cal = Calculator()
+		assert 2 == self.cal.add(1, 1)
+
+
+	@pytest.mark.add
+	def test_add1(self):
+		#cal = Calculator()
+		assert 3 == self.cal.add(1, 2)
+
+
+	@pytest.mark.div
+	def test_div(self):
+		#cal = Calculator()
+		assert 1 == self.cal.div(1, 1)
