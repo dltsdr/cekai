2e35fb7fb80c3ff0866cee35aae0a905d41be77d
dltsdr <852668587@qq.com>
2020-12-21 20:28:43 +0800

    重写

---
+++ testing/demo1/demo11/test_demo11.py
@@ -0,0 +1,2 @@
+def test_case11(login):
+    print("case11")
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,53 +1,18 @@
-# 测试文件
-
-import sys, pytest
-
-print(sys.path.append('..'))
-
+#实现计算机
+import sys,pytest
+sys.path.append('..')
 from pythoncode.calc import Calculator
 
+@pytest.mark.add
+def test_add():
+	cal = Calculator()
+	assert 2 == cal.add(1,1)
 
-def setup_module():
-	print("模块级别setup")
+def test_add1():
+	cal = Calculator()
+	assert 3 == cal.add(1,2)
 
-def teardown_module():
-	print("函数级别teardown")
-#函数级别
-def setup_function():
-	print("函数级别setup")
-
-def teardown_function():
-	print("函数级别teardown")
-
-
-class TestCalc():
-	def setup_class(self):
-		self.cal = Calculator
-		print("类级别setup")
-
-	def teardown_class(self):
-		print("类级别teardowns")
-
-	def setup(self):
-		print("setup")
-
-	def teardown(self):
-		print("teardown")
-
-	@pytest.mark.add
-	@pytest.mark.parametrize('a,b,result',[(1,1,2),(2,2,4)])
-	def test_add(self, a, b, result):
-		#cal = Calculator()
-		assert result == self.cal.add(a, b)
-
-
-	@pytest.mark.add
-	def test_add1(self):
-		#cal = Calculator()
-		assert 3 == self.cal.add(1, 2)
-
-
-	@pytest.mark.div
-	def test_div(self):
-		#cal = Calculator()
-		assert 1 == self.cal.div(1, 1)
+@pytest.mark.div
+def test_div():
+	cal = Calculator()
+	assert 3 == cal.div(1,1)
