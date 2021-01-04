857989e5019f7fa00e39f631e25b917779e49cdf
dltsdr <852668587@qq.com>
2021-01-04 23:33:33 +0800

    test

---
+++ homework/pyhomework/pythoncode/calc.py
@@ -0,0 +1,14 @@
+# 计算器
+# 被测文件
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mult(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
