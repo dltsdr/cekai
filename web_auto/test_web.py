5b98c62b51ff40ce957e9523cc349b7dfb71a040
dltsdr <852668587@qq.com>
2021-01-10 17:00:48 +0800

    web作业

--- web_auto/test_web.py
+++ web_auto/test_web.py
@@ -2,6 +2,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import yaml
 
 class Test():
     def setup_method(self,method):
@@ -9,6 +10,7 @@ class Test():
         #和浏览器打开的调试端口进行调试
         options.debugger_address="127.0.0.1:9222"
         self.driver = webdriver.Chrome(options=options)
+        #self.driver = webdriver.Chrome()
 
     # def teardown_method(self,method):
     #     sleep(5)
@@ -21,14 +23,19 @@ class Test():
 
     def test_wework(self):
         self.driver.get("https://work.weixin.qq.com/")
-        cookies = [{'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.vid', 'path': '/', 'secure': False, 'value': '1688851890906062'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.vid', 'path': '/', 'secure': False, 'value': '1688851890906062'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.corpid', 'path': '/', 'secure': False, 'value': '1970324953149697'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.sid', 'path': '/', 'secure': False, 'value': 'TMysB7Kdx3rZLJ2aGu87OTsZ7j6Lifdua0FjUjbKYnvUjA2Ira1KHEYXONVtpQd6'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.d2st', 'path': '/', 'secure': False, 'value': 'a1782055'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.vst', 'path': '/', 'secure': False, 'value': 'mdm_8VuoKfXP7Zrb4_5PidqP7TDCH3ogaTUvwgvK0ImkhHtpDtz2a2HMREfvepD8aeyDAwM4v3QDLtl8u4mGHfRL2k9g0B7mJjGfeSCn7ebOpITaesnX8kWsQnRo14X3oUAEShL5THOaMG38oykYaClh-EkX4cKRrGeyR-IZmUMhzsa1G9yLeZmVaDZ0JW8Ze77jaFGl4IrbFI2qBl4ihEfb_Wgxc8DXCDogqoJYo5jAhEd3_d7kPQAUg8j7bhnGQSG4A76LR0r509ohH_JSTQ'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'Hm_lpvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': False, 'value': '1609935648'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.refid', 'path': '/', 'secure': False, 'value': '319385823288795'}, {'domain': 'work.weixin.qq.com', 'expiry': 1609963870, 'httpOnly': True, 'name': 'ww_rtkey', 'path': '/', 'secure': False, 'value': '1q52ig0'}, {'domain': '.qq.com', 'expiry': 1610021923, 'httpOnly': False, 'name': '_gid', 'path': '/', 'secure': False, 'value': 'GA1.2.1481080812.1609932344'}, {'domain': '.qq.com', 'expiry': 1924082746, 'httpOnly': False, 'name': 'pac_uid', 'path': '/', 'secure': False, 'value': '0_8623295166533'}, {'domain': '.qq.com', 'expiry': 1637201575, 'httpOnly': False, 'name': 'eas_sid', 'path': '/', 'secure': False, 'value': 'R1C6Q0y5v6l6j585X765s8k8O5'}, {'domain': '.work.weixin.qq.com', 'expiry': 1612527648, 'httpOnly': False, 'name': 'wwrtx.i18n_lan', 'path': '/', 'secure': False, 'value': 'zh'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ltype', 'path': '/', 'secure': False, 'value': '1'}, {'domain': '.work.weixin.qq.com', 'expiry': 1641471648, 'httpOnly': False, 'name': 'Hm_lvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': False, 'value': '1609932344,1609933019,1609933683'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ref', 'path': '/', 'secure': False, 'value': 'direct'}, {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvid', 'path': '/', 'secure': False, 'value': '8193769266'}, {'domain': '.qq.com', 'expiry': 2147385600, 'httpOnly': False, 'name': 'pgv_pvi', 'path': '/', 'secure': False, 'value': '7133389824'}, {'domain': '.qq.com', 'expiry': 2147483515, 'httpOnly': False, 'name': 'ptcz', 'path': '/', 'secure': False, 'value': '8acfe18b21b478402a9617177b88a778616caf2469ecd7eb79d672bbc8014ec3'}, {'domain': '.qq.com', 'expiry': 2147483569, 'httpOnly': False, 'name': 'RK', 'path': '/', 'secure': False, 'value': 'kB7QKdzGND'}, {'domain': '.qq.com', 'expiry': 1673007523, 'httpOnly': False, 'name': '_ga', 'path': '/', 'secure': False, 'value': 'GA1.2.1568189764.1604302372'}, {'domain': '.work.weixin.qq.com', 'expiry': 1635838370, 'httpOnly': False, 'name': 'wwrtx.c_gdpr', 'path': '/', 'secure': False, 'value': '0'}]
+        cookies = [{'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.vid', 'path': '/', 'secure': False, 'value': '1688851890906062'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.vst', 'path': '/', 'secure': False, 'value': 'jb9uD8ZUYhgAHsaqXLaJ9cidktcCWy-eCLyWh3rV7JAeXff0ludXbfRmcQkbQuAf7qXvJqr8zVMgDIM5rxE-bTHJP5f7Sb4EE3qOwyw9TqovdLudV95DNk-pVfXYWXxM2iKeI-lDW3bWNS2yg09p4aPb9l-BZBMAlmP_vEVm63Ikjfu7rDFrDp-LuNHKgpmHwA_UBD593pzgsoxNSznVNf980Ia9vHTkGtLd03DiVRkqxioUfnpPNtB0KufWAPpebOITSIvap3boEN9s1N21fQ'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.vid', 'path': '/', 'secure': False, 'value': '1688851890906062'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wxpay.corpid', 'path': '/', 'secure': False, 'value': '1970324953149697'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.sid', 'path': '/', 'secure': False, 'value': 'TMysB7Kdx3rZLJ2aGu87ObKTnJ6354BgzkrRXYBe0e2OYIjLk7MSOmxGji00ymu9'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'wwrtx.d2st', 'path': '/', 'secure': False, 'value': 'a5798250'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ltype', 'path': '/', 'secure': False, 'value': '1'}, {'domain': '.work.weixin.qq.com', 'expiry': 1641697907, 'httpOnly': False, 'name': 'Hm_lvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': False, 'value': '1609932344,1609933019,1609933683,1610161907'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.ref', 'path': '/', 'secure': False, 'value': 'direct'}, {'domain': '.work.weixin.qq.com', 'httpOnly': False, 'name': 'Hm_lpvt_9364e629af24cb52acc78b43e8c9f77d', 'path': '/', 'secure': False, 'value': '1610161907'}, {'domain': '.work.weixin.qq.com', 'httpOnly': True, 'name': 'wwrtx.refid', 'path': '/', 'secure': False, 'value': '1967743098705182'}, {'domain': '.qq.com', 'expiry': 1610248322, 'httpOnly': False, 'name': '_gid', 'path': '/', 'secure': False, 'value': 'GA1.2.1481080812.1609932344'}, {'domain': 'work.weixin.qq.com', 'expiry': 1610191733, 'httpOnly': True, 'name': 'ww_rtkey', 'path': '/', 'secure': False, 'value': '3c5h7nb'}, {'domain': '.qq.com', 'expiry': 1610161967, 'httpOnly': False, 'name': '_gat', 'path': '/', 'secure': False, 'value': '1'}, {'domain': '.work.weixin.qq.com', 'expiry': 1641697906, 'httpOnly': False, 'name': 'wwrtx.c_gdpr', 'path': '/', 'secure': False, 'value': '0'}, {'domain': '.qq.com', 'expiry': 1673233922, 'httpOnly': False, 'name': '_ga', 'path': '/', 'secure': False, 'value': 'GA1.2.1568189764.1604302372'}, {'domain': '.work.weixin.qq.com', 'expiry': 1612753924, 'httpOnly': False, 'name': 'wwrtx.i18n_lan', 'path': '/', 'secure': False, 'value': 'zh'}]
         print(self.driver.get_cookies())
         #expiry是cookie过期字段，去掉此字段cookie会生效
         for cookie in cookies:
             if "expiry" in cookie.keys():
                 cookie.pop("expiry")
-
             #把字典加入driver的cookie
             self.driver.add_cookie(cookie)
-
         self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")
+
+    def test_get_cookies(self):
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")
+        cookie = self.driver.get_cookies()
+        print(cookie)
+        with open("cookies.yaml","w",encoding="UTF-8") as f:
+            yaml.dump(cookie,f)
