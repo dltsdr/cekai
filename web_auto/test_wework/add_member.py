280cc4278fee693c9745a8e9eb40e817a1bcb6c7
dltsdr <852668587@qq.com>
2021-01-13 23:47:39 +0800

    po

--- web_auto/test_wework/add_member.py
+++ web_auto/test_wework/add_member.py
@@ -1,14 +1,16 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from web_auto.test_wework.base_page import BasePage
 
 
-class AddMember:
-
-    def __init__(self, driver:WebDriver):
-        self.driver = driver
+class AddMember(BasePage):
 
     def add_member(self):
-        dr = self.driver
+        """
+        添加成员页面，实现成员添加
+        :return:
+        """
+        dr = self._driver
         dr.find_element(By.ID, "username").send_keys("D")
         dr.find_element(By.ID, "memberAdd_acctid").send_keys("16234")
         dr.find_element(By.ID, "memberAdd_phone").send_keys("17347638617")
--- web_auto/test_wework/index.py
+++ web_auto/test_wework/index.py
@@ -1,27 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
 from web_auto.test_wework.add_member import AddMember
+from web_auto.test_wework.base_page import BasePage
 
 
-class Index:
-    def __init__(self):
-        chrome_options = Options()
-        chrome_options.debugger_address = "127.0.0.1:9222"
-        self.driver = webdriver.Chrome(options=chrome_options)
-        #self.driver = webdriver.Chrome()
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
-        sleep(3)
+class Index(BasePage):
+    _base_url = "https://work.weixin.qq.com/wework_admin/frame#index"
 
     def goto_add_member(self):
         """
         添加成员index_service_cnt_itemWrap
         :return:
         """
-        self.driver.find_element(By.CSS_SELECTOR,".index_service_cnt_itemWrap").click()
+
+        self._driver.find_element(By.CSS_SELECTOR,".index_service_cnt_itemWrap").click()
         sleep(3)
-        return AddMember(self.driver)
+        return AddMember(self._driver)
         pass
 
     def goto_import_address(self):
