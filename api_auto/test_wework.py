4cba875e558683e78a6a5c4f238ae8673c09f3e8
dltsdr <852668587@qq.com>
2021-02-24 12:53:14 +0800

    po

--- api_auto/test_wework.py
+++ api_auto/test_wework.py
@@ -2,11 +2,40 @@ import requests
 
 class TestWework:
     #获取token
-    def test_get_token(self):
+    def get_token(self):
         r = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wwdf8d39f6d699b1f3&corpsecret=8fj9_sHJFZwqgGX6Ks6HCJ-a9P52W4g2wMizEMr6UWg")
         print(r.json())
         return r.json()["access_token"]
 
     #创建成员
     def test_create(self):
-        requests.post("https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token=ACCESS_TOKEN")
+        access_token = self.get_token()
+        requests_body = {
+            "userid": "zhangsan",
+            "name": "张三",
+            "mobile": "13800000000",
+            "department": [1]}
+
+        r = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={access_token}",
+                      json=requests_body
+                      )
+        print(r.json())
+
+    #获取成员
+    def test_get(self):
+        access_token = self.get_token()
+        r = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={access_token}&userid=DouXinPeng")
+        print(r.json())
+
+
+    #更新成员
+    def test_update(self):
+        access_token = self.get_token()
+        requests_body = {
+            "userid": "DuanZhiYu",
+            "position": "TESTER"}
+
+        r = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={access_token}",
+                      json=requests_body
+                      )
+        print(r.json())
