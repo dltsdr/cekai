1b9db6528aaca3e8a21b8a293002ec23234d3deb
dltsdr <852668587@qq.com>
2021-02-24 14:14:19 +0800

    po

--- api_auto/test_wework.py
+++ api_auto/test_wework.py
@@ -2,14 +2,19 @@ import requests
 
 class TestWework:
     #获取token
-    def get_token(self):
-        r = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=wwdf8d39f6d699b1f3&corpsecret=8fj9_sHJFZwqgGX6Ks6HCJ-a9P52W4g2wMizEMr6UWg")
+    def test_get_token(self):
+        requests_params = {
+            "corpid":"wwdf8d39f6d699b1f3",
+            "corpsecret":"8fj9_sHJFZwqgGX6Ks6HCJ-a9P52W4g2wMizEMr6UWg"}
+
+        r = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken",
+                         params=requests_params)
         print(r.json())
         return r.json()["access_token"]
 
     #创建成员
     def test_create(self):
-        access_token = self.get_token()
+        access_token = self.test_get_token()
         requests_body = {
             "userid": "zhangsan",
             "name": "张三",
@@ -23,7 +28,7 @@ class TestWework:
 
     #获取成员
     def test_get(self):
-        access_token = self.get_token()
+        access_token = self.test_get_token()
         r = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={access_token}&userid=DouXinPeng")
         print(r.json())
 
