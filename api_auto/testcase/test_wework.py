467f69cd1e0f9ede5bf0efa59f0e7dbcf6ee1b23
dltsdr <852668587@qq.com>
2021-03-09 23:36:01 +0800

    po

--- api_auto/testcase/test_wework.py
+++ api_auto/testcase/test_wework.py
@@ -1,5 +1,7 @@
 from api_auto.api.wework import WeWork
+from api_auto.api.util import Util
 import requests
+
 
 class TestWork:
     #获取成员
@@ -20,3 +22,10 @@ class TestWork:
 
     def test_session(self):
         s = requests.session()
+
+    #生成一次token，可作用于所有用例，下面举例是get取得用户信息
+    def test_session(self):
+        s = requests.session()
+        s.params = {"access_token":Util().get_token()}
+        res = s.get("https://qyapi.weixin.qq.com/cgi-bin/user/get?userid=DuanZhiYu")
+        print(res.json())
